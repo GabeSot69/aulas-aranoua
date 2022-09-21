2499f2628a8bab437bf90e581a395b725dd1e208
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-09-21 00:39:33 -0400

    Delete Aranoua/src/aranoua/com/inferfacedimensao directory

--- Aranoua/src/aranoua/com/inferfacedimensao/InterfaceDimensao.java
+++
@@ -1,7 +0,0 @@
-package aranoua.com.interfacedimensao;
-
-public interface InterfaceDimensao {
-	int TAMANHO = 2;
-	public void imprimir();
-	public void adicionar(int valor) throws MyClassException;
-}
--- Aranoua/src/aranoua/com/inferfacedimensao/MyClassException.java
+++
@@ -1,8 +0,0 @@
-package aranoua.com.interfacedimensao;
-
-public class MyClassException extends Exception{
-	public String tamanhoUltrapassado() {
-		return "Tamanho excedido\nSe continuar havera sobreposicao de conteudo";
-	}
-	public String tamanhoInvalido(){ return "Tamanho invalido"; }
-}
