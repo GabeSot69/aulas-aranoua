c051be7456e629cc9fb812bc315572ae8ca3488c
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-09-24 11:53:39 -0400

    Add files via upload

---
+++ GUI/src/com/ifam/gui/TestaExemplos.java
@@ -0,0 +1,12 @@
+package com.ifam.gui;
+
+import javax.swing.JFrame;
+
+public class TestaExemplos {
+	public static void main(String[] args) {
+		Exemplos ex = new Exemplos();
+        ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ex.setSize(375,210);
+        ex.setVisible(true);
+	}
+}
---
+++ GUI/src/com/ifam/gui/TestaJLabel.java
@@ -0,0 +1,14 @@
+package com.ifam.gui;
+
+import javax.swing.JFrame;
+
+public class TestaJLabel {
+
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(260,220);
+        labelFrame.setVisible(true);
+    }
+
+}
